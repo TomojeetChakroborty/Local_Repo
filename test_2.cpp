befdf2857bf8577bf516c5f17e166ebc9e6cc720
TomojeetChakroborty <tomochak4849@gmail.com>
2023-08-29 12:47:26 +0530

    1st up

---
+++ test_2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+// Partition the array into two subarrays: elements <= pivot and elements > pivot
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high]; // Choose the last element as the pivot
+    int i = low - 1; // Index of the smaller element
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1; // Index of the pivot after partition
+}
+
+// Recursive function to perform QuickSort
+void quickSort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+int main() {
+    std::vector<int> arr = {10, 7, 8, 9, 1, 5};
+
+    std::cout << "Original array: ";
+    for (int num : arr)
+        std::cout << num << " ";
+    std::cout << std::endl;
+
+    quickSort(arr, 0, arr.size() - 1);
+
+    std::cout << "Sorted array: ";
+    for (int num : arr)
+        std::cout << num << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
